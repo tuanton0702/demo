47717f6bcdefa96297f8bb525c4b025e006bb4fc
TuanTon <thattuan15es@gmail.com>
2020-09-11 22:26:29 +0700

    first commit

---
+++ ClassTest.py
@@ -0,0 +1,10 @@
+class SieuNhan:
+    def __init__(self,ten,vukhi,mausac):
+        self.ten=ten
+        self.vukhi=vukhi
+        self.mausac=mausac
+pass
+sieunhando=SieuNhan("GaoRed","Kiem","Do")
+sieunhanxanh=SieuNhan("GaoBlue","Sung","Xanh")
+print(" {} dang thong dit {}".format(sieunhando.ten,sieunhanxanh.ten))
+print("{} la vu khi cua {} ".format(sieunhanxanh.vukhi,sieunhanxanh.ten))
